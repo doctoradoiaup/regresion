8769a90af8c3801c65d0ecd318c68dd333bc7396
doctoradoiaup <doctoradoiaup@gmail.com>
2024-10-30 13:06:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,3 @@ st.subheader("Desarrollado por: Javier Horacio Pérez Ricárdez")
 st.subheader("Catedrático: Félix Orlando Martínez")
 
 # Agregar contenido adicional según sea necesario
-st.write("Esta aplicación permite explorar diferentes modelos de regresión.")
