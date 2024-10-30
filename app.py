37007e9f5474ab665307d910408f410679141177
doctoradoiaup <doctoradoiaup@gmail.com>
2024-10-30 12:53:59 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Título de la aplicación
+st.title("Modelos de Regresión")
+
+# Información adicional
+st.header("Materia: Aprendizaje Máquina I")
+st.subheader("Desarrollado por: Javier Horacio Pérez Ricárdez")
+st.subheader("Catedrático: Félix Orlando Martínez")
+
+# Agregar contenido adicional según sea necesario
+st.write("Esta aplicación permite explorar diferentes modelos de regresión.")
